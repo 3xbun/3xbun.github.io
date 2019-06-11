a728fc6f39c4e2efec2dfa920456c1826e074d98
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2019-06-11 10:35:24 +0700

    Current class now response to each links

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-const menu = document.querySelector("#menu")
-const button = document.querySelector(".button")
-var state = true
+const menu = document.querySelector("#menu");
+const button = document.querySelector(".button");
+var state = true;
 
 const toggleMenu = () => {
   if (state) {
-    button.style.transform = "rotate(45deg)"
-    menu.style.transform = "translateX(0%)"
-    state = false
+    button.style.transform = "rotate(45deg)";
+    menu.style.transform = "translateX(0%)";
+    state = false;
   } else {
-    button.style.transform = "rotate(90deg)"
-    menu.style.transform = "translateX(100%)"
-    state = true
+    button.style.transform = "rotate(90deg)";
+    menu.style.transform = "translateX(100%)";
+    state = true;
   }
-}
+};
+
+const changeHighlight = href => {
+  let link = href.replace("#", "");
+
+  let newCurrent = document.querySelectorAll(`${href}-link`);
+  let oldCurrent = document.querySelectorAll(".current");
+  oldCurrent.forEach(link => link.classList.remove("current"));
+  newCurrent.forEach(link => link.classList.add("current"));
+
+  // console.log(newCurrent);
+};
